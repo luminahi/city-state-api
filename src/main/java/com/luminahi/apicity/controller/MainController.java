483d822d66dc4bc9572f57cda47a0583b755cfac
alex lima <delimaferreiraalex@gmail.com>
2024-03-13 20:09:40 -0300

    add file to persist logs

--- src/main/java/com/luminahi/apicity/controller/MainController.java
+++ src/main/java/com/luminahi/apicity/controller/MainController.java
@@ -1,14 +1,18 @@
 package com.luminahi.apicity.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    
+
+    private final static Logger log = LoggerFactory.getLogger(MainController.class);
+
     @GetMapping("/")
     public String redirectTo() {
+        log.info("user redirected to /cidades");
         return "redirect:/cidades";
     }
-    
 }
