c3f3f6a3fcc3b38beb04ddf626932e72be50dbf8
alex lima <delimaferreiraalex@gmail.com>
2024-03-19 19:06:22 -0300

    testing the test

--- src/test/java/com/luminahi/apicity/CityStateApiApplicationTests.java
+++ src/test/java/com/luminahi/apicity/CityStateApiApplicationTests.java
@@ -1,5 +1,8 @@
 package com.luminahi.apicity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,17 @@ class CityStateApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
+	
+	@Test
+	void testingEquals() {
+	    assertEquals(1, 1);
+	}
+	
+   @Test
+    void testingFalse() {
+       assertFalse(false);
+    }
 
 }
